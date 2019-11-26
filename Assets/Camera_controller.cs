711a9c8c0da9148abc26a7492406c79e1fe7ec04
fanxuanliao <106703008@nccu.edu.tw>
2019-11-26 23:06:38 +0800

    191126

--- Assets/Camera_controller.cs
+++ Assets/Camera_controller.cs
@@ -13,6 +13,9 @@ public class Camera_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //鏡頭跟著滑鼠移動
+        //鏡頭跟著滑鼠移動'
+        float view_y = 1f * Input.GetAxis("Mouse Y");
+        transform.Rotate(-view_y, 0, 0);
+
     }
 }
--- Assets/Player_controller.cs
+++ Assets/Player_controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player_controller : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,15 @@ public class Player_controller : MonoBehaviour
     void Update()
     {
         //基本角色移動
+        float mov_x = Input.GetAxisRaw("Horizontal");
+        float mov_z = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.right * mov_x * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * mov_z * speed * Time.deltaTime);
+        float view_x = 1f * Input.GetAxis("Mouse X");
+        
+        transform.Rotate(0, view_x, 0);
+        //
+
     }
 
 
