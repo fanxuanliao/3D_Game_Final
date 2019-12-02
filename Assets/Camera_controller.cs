fa5d538fe7104b6ca0337881cde24d90fa215026
fanxuanliao <106703008@nccu.edu.tw>
2019-12-02 12:18:17 +0800

    redo items

--- Assets/Camera_controller.cs
+++ Assets/Camera_controller.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Camera_controller : MonoBehaviour
 {
-
-    public float Camera_Move = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +14,8 @@ public class Camera_controller : MonoBehaviour
     void Update()
     {
         //鏡頭跟著滑鼠移動'
-        float view_y = Input.GetAxis("Mouse Y");
-        transform.Rotate(-view_y * Camera_Move, 0, 0);
+        float view_y = 1f * Input.GetAxis("Mouse Y");
+        transform.Rotate(-view_y, 0, 0);
 
     }
 }
--- Assets/Selection_Manager.cs
+++ Assets/Selection_Manager.cs
@@ -7,10 +7,7 @@ public class Selection_Manager : MonoBehaviour
     [SerializeField]private Material interactive_hl;
     [SerializeField]private Material checking_hl;
     [SerializeField] private Material default_material;
-<<<<<<< HEAD
-=======
     private Color default_color;
->>>>>>> 25948696eaebc0d213a19ebdf656aa1177ab637d
     private Transform _selection;
     float raylength;
 
@@ -23,16 +20,6 @@ public class Selection_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        if(_selection != null)
-        {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material = default_material;
-            _selection = null;
-        }
-        var mouse = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        RaycastHit hit;
-=======
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 2;
         Debug.DrawRay(transform.position, forward, Color.yellow);
         if(_selection != null)
@@ -46,16 +33,12 @@ public class Selection_Manager : MonoBehaviour
         var mouse = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         RaycastHit hit;
         //射線
->>>>>>> 25948696eaebc0d213a19ebdf656aa1177ab637d
         if(Physics.Raycast(mouse, out hit, raylength))
         {
             var selection = hit.transform;
             if(selection.CompareTag("interactive"))
             {
-<<<<<<< HEAD
-=======
             //    print("interactive");
->>>>>>> 25948696eaebc0d213a19ebdf656aa1177ab637d
                 var selectionRenderer = selection.GetComponent<Renderer>();
                 if(selectionRenderer != null)
                 {
@@ -76,9 +59,6 @@ public class Selection_Manager : MonoBehaviour
                 _selection = selection;
             }
         }
-<<<<<<< HEAD
-=======
         //射線互動
->>>>>>> 25948696eaebc0d213a19ebdf656aa1177ab637d
     }
 }
