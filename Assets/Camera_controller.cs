a04e64ce76b93f35efbd0a887358c1c56f473617
ewailiao <57829701+ewailiao@users.noreply.github.com>
2019-12-08 02:15:09 +0800

    Update Camera_controller.cs

--- Assets/Camera_controller.cs
+++ Assets/Camera_controller.cs
@@ -18,6 +18,9 @@ public class Camera_controller : MonoBehaviour
         //鏡頭跟著滑鼠移動'
         float view_y = Camera_move * Input.GetAxis("Mouse Y");
         transform.Rotate(-view_y, 0, 0);
+        
+        Cursor.visible = false;//隱藏滑鼠
+        Cursor.lockState = CursorLockMode.Locked;//把滑鼠鎖定到螢幕中間
 
     }
 }
