95666eeab7dbd2b8b3c7f049e73baad58cddaf59
ewailiao <ewailiao@gmail.com>
2020-01-16 16:27:03 +0800

    menu complete

---
+++ Assets/tutorial_botton_click.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial_botton_click : MonoBehaviour
+{
+
+    public GameObject tutorial_image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void active_image()
+    {
+        tutorial_image.SetActive(true);
+    }
+}
